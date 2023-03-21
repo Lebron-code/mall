3d33031e26eecd66c09c499ddabe944088cc3c72
Lebron <1059094918@qq.com>
2023-03-21 21:24:28 +0800

    项目初始结构

---
+++ mall-coupon/src/test/java/com/lwh/coupon/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwh.coupon.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
